b1df94d79b0af77472821b9ca5ea99c9c433e6ed
Eric Koerlin <ekoerlin@mymail.mines.edu>
2015-02-25 17:38:57 -0700

    First two parts

--- unit07/db/schema.rb
+++ unit07/db/schema.rb
@@ -13,4 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20150225222938) do
 
+  create_table "pets", force: :cascade do |t|
+    t.string   "name"
+    t.string   "age"
+    t.string   "description"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "pet_type",    default: 0, null: false
+  end
+
 end
--- unit07/db/seeds.rb
+++ unit07/db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Pet.create!([
+  {name: "Test", age: "12", description: "A sweet old dog", pet_type: 0},
+  {name: "Fluffy", age: "45", description: "A very olde cat", pet_type: 1},
+	{name: "Tabby", age: "3", description: "A young tabby cat, very sweet", pet_type: 1},
+	{name: "Fido", age: "5", description: "A feisty beagle, loves to play", pet_type: 0},
+	{name: "Lucy", age: "1", description: "A sweet black and grey kitten", pet_type: 1},
+	{name: "Tazz", age: "4", description: "A ferocious ridgeback", pet_type: 0}
+])
